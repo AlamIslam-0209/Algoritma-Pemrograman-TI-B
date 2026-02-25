9565e1b670bc7157dc11f745245a1bd673a20d2a
Alam <alamganteng8250@gmail.com~>
2026-02-25 19:19:52 +0700

    Pertemuan kelima

---
+++ Islam Rahmatan Lil`Alamin_25071100662_TI-B/Pertemuan-5/Latihan/matriks.py
@@ -0,0 +1,59 @@
+A = [[5, 3, 1],
+     [2, 8, 4],
+     [6, 0, 7]]
+
+B = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+
+C = [[0 for i in range(3)] for j in range(3)]
+C_tambah = [[0 for i in range(3)] for j in range(3)]
+C_kurang = [[0 for i in range(3)] for j in range(3)]
+C_skalar = [[0 for i in range(3)] for j in range(3)]
+
+
+for baris in range(len(A)):
+    for elemen in range(len(A[baris])):
+        C_tambah[baris][elemen] = A[baris][elemen] + B[baris][elemen]
+    print(C_tambah[baris], end="\n")
+    
+print()
+
+for baris in range(len(A)):
+    for elemen in range(len(A[baris])):
+        C_kurang[baris][elemen] = A[baris][elemen] - B[baris][elemen]
+    print(C_tambah[baris], end="\n")
+
+print()
+
+for baris in range(len(A)):
+    for elemen in range(len(A[baris])):
+        C_skalar[baris][elemen] = A[baris][elemen] * 4
+    print(C_tambah[baris], end="\n")
+print()
+
+
+#===================================================================#
+
+print("===================================================================")
+print("Versi pakai numpy \n")
+import numpy as np
+
+A = np.array(A)
+B = np.array(B)
+
+C_tambah = np.array(A) + np.array(B)
+print(C_tambah)
+
+C_kurang = np.array(A) - np.array(B)
+print(C_kurang)
+
+C_skalar = np.array(A) * 4
+print(C_skalar)
+
+C_dot = np.dot(A, B)
+print(C_dot)
+
+A_tranpose = A.T
+B_tranpose = B.T
